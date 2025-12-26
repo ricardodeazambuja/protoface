5eb9cfb20c3ca57e72e8724c44229eda0c494feb
ricardodeazambuja <ricardo.azambuja@gmail.com>
2025-12-26 16:23:36 -0500

    Improved emotion distinction

--- src/utils/emotions.js
+++ src/utils/emotions.js
@@ -4,7 +4,7 @@ export const emotionParams = {
     joy: [2.8, 0.1, 0.3, 0.9, 1.0, 0.0, 0.0, 0.3, 0.3], // Max U-shape (high s, high m, high q/r)
     glee: [0.0, 0.5, 0.9, 0.7, 1.0, 0.0, 0.0, 0.4, -0.4],
     surprised: [3.0, 0.0, 0.0, 1.0, 0.5, 0.0, 0.0, 0.8, 0.8], // Perfect vertical oval (high m, high r for roundness)
-    shock: [3.0, -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.5, 0.5], // Large open frown (high m, negative p/q for frown shape)
+    shock: [3.0, 0.0, 0.0, 0.9, 0.3, 0.0, 0.0, 0.9, 0.9], // Wide "O" mouth (high r for roundness, slight s for shape), very raised brows
     worried: [1.0, 1.0, 0.75, 0.7, 0.1, 0.0, 0.0, 0.0, 0.3],
     sad: [2.0, -0.2, -0.8, 0.6, 0.0, 0.0, 0.0, 0.4, 0.5], // Deep inverted frown (low s, high m)
     angry: [0.8, 0.3, 0.5, 0.9, 0.1, 0.0, 0.0, -0.6, -1.0], // Squared-off gritted teeth (high r, low s)
